94068e55b29195ac2bbd464adc623562de35853b
Yang Zhuohan <billstark1996@gmail.com>
2017-03-01 22:17:46 +0800

    initialization

---
+++ lAyeR/lAyeR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lAyeR
+//
+//  Created by BillStark on 3/1/17.
+//  Copyright © 2017 nus.cs3217.layer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
