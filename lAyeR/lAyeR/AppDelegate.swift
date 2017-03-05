02f921db8993599b0493d6210d65a541657438b3
Duan Yichen <victoriaduan@VictoriadeMacBook-Air.local>
2017-03-05 18:04:03 +0900

    add google api

--- lAyeR/lAyeR/AppDelegate.swift
+++ lAyeR/lAyeR/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAxEeB1jYBx9HgghM4IXxxzGIA4p6yjr9s")
+        GMSPlacesClient.provideAPIKey("AIzaSyBT4bqEv5pABPznq2HsPlIuVVcdJXgV3JY")
         return true
     }
 
--- lAyeR/lAyeR/ViewController.swift
+++ lAyeR/lAyeR/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var placesClient: GMSPlacesClient?
+    let locationManger: CLLocationManager = CLLocationManager()
+    
+    // Instantiate a pair of UILabels in Interface Builder
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManger.delegate = self
+        locationManger.requestAlwaysAuthorization()
+        locationManger.requestWhenInUseAuthorization()
+        placesClient = GMSPlacesClient()
+    }
+    
+    // Add a UIButton in Interface Builder to call this function
+    @IBAction func getLocation(_ sender: Any) {
+        placesClient?.currentPlace(callback: {
+            (placeLikelihoodList: GMSPlaceLikelihoodList?, error: Error?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLicklihoodList = placeLikelihoodList {
+                let place = placeLicklihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress
+                }
+            }
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
