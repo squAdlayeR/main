4c685167bb5ff9fa0e08c4d4d7acd5693b53ad3d
Yang Zhuohan <billstark1996@gmail.com>
2017-03-29 22:12:22 +0800

    redesign login view controller

--- lAyeR/lAyeR/LoginViewController.swift
+++ lAyeR/lAyeR/LoginViewController.swift
@@ -9,13 +9,111 @@
 import FirebaseAuth
 import UIKit
 
+/**
+ This is a view controller specially for user login.
+ This is the first view controller that user will be seeing.
+ */
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var emailField: UITextField!
+    // Connects outlets of sample input fields
+    @IBOutlet weak var emailFieldSample: UITextField!
+    @IBOutlet weak var passwordFieldSample: UITextField!
     
-    @IBOutlet weak var passwordField: UITextField!
+    // Connects welcome title and subtitle
+    @IBOutlet weak var welcomeTitle: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
     
+    // Connects register hint
+    @IBOutlet weak var registerHint: UILabel!
+    
+    // Connects buttons
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    // Defines the view for vibrancy effect
+    var vibrancyEffectView: UIVisualEffectView!
+    
+    // Defines the real input fields
+    var emailField: InputTextFeild!
+    var passwordField: InputTextFeild!
+    
+    // Data service instance, used to validate login
     let dataService = DataServiceManager.instance
+    
+    override func viewDidLoad() {
+        setupCameraView()
+        setupBlurEffect()
+        setupText()
+        setupFormInput()
+        setupButtons()
+    }
+    
+    private func setupCameraView() {
+        let cameraViewController = CameraViewController()
+        cameraViewController.view.frame = view.bounds
+        view.addSubview(cameraViewController.view)
+    }
+    
+    private func setupBlurEffect() {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        let vibrancyEffectView = UIVisualEffectView(effect: UIVibrancyEffect(blurEffect: blurEffect))
+        
+        blurEffectView.frame = view.bounds
+        vibrancyEffectView.frame = blurEffectView.bounds
+        
+        self.vibrancyEffectView = vibrancyEffectView
+        blurEffectView.contentView.addSubview(self.vibrancyEffectView)
+        view.addSubview(blurEffectView)
+    }
+    
+    private func setupText() {
+        view.addSubview(welcomeTitle)
+        vibrancyEffectView.contentView.addSubview(subtitle)
+        vibrancyEffectView.contentView.addSubview(registerHint)
+    }
+    
+    private func setupFormInput() {
+        setupEmailInput()
+        setupPasswordInput()
+    }
+    
+    private func setupEmailInput() {
+        emailField = createTextFeild(with: emailFieldSample, and: "email address")
+        emailField.keyboardType = .emailAddress
+        emailField.delegate = self
+        vibrancyEffectView.contentView.addSubview(emailField)
+    }
+    
+    private func setupPasswordInput() {
+        passwordField = createTextFeild(with: passwordFieldSample, and: "password")
+        passwordField.isSecureTextEntry = true
+        passwordField.delegate = self
+        vibrancyEffectView.contentView.addSubview(passwordField)
+    }
+    
+    private func setupButtons() {
+        setupLoginButton()
+        setupRegisterButton()
+    }
+    
+    private func setupLoginButton() {
+        loginButton.layer.cornerRadius = loginButton.bounds.height / 2
+        loginButton.layer.masksToBounds = true
+        view.addSubview(loginButton)
+    }
+    
+    private func setupRegisterButton() {
+        registerButton.titleLabel?.textColor = UIColor.yellow
+        vibrancyEffectView.contentView.addSubview(registerButton)
+    }
+    
+    private func createTextFeild(with sample: UITextField, and placeHolder: String) -> InputTextFeild {
+        let inputSize = CGSize(width: sample.bounds.width, height: 60)
+        let newTextFeild = InputTextFeild(placeHolder: placeHolder, size: inputSize)
+        newTextFeild.center = sample.center
+        return newTextFeild
+    }
     
     @IBAction func signInUser(_ sender: Any) {
         let email = emailField.text ?? ""
@@ -54,10 +152,26 @@ class LoginViewController: UIViewController {
         }
     }
     
-    func showErrorAlert(message: String) {
+    private func showErrorAlert(message: String) {
         let alert = UIAlertController(title: "Oops", message: message, preferredStyle: UIAlertControllerStyle.alert)
         let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+    
+}
+
+extension LoginViewController {
+    
+    
+    
 }
