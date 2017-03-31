a7c92d9fc748aea203065e83232715fbb84ff1f3
Yang Zhuohan <billstark1996@gmail.com>
2017-03-31 22:12:45 +0800

    finish abstraction & documentation

--- lAyeR/lAyeR/UserProfileViewController.swift
+++ lAyeR/lAyeR/UserProfileViewController.swift
@@ -8,45 +8,58 @@
 
 import UIKit
 
+/**
+ This is a view controller for displaying user infomation, which includes
+ - User name, user location, user avatar
+ - List of routes that are designed by this user
+    - route name
+    - route screen shot
+    - route description
+ These information could be retrieved when view is loaded
+ */
 class UserProfileViewController: UIViewController {
 
-    var vibrancyEffectView: UIVisualEffectView!
-    let testData = [["Walk from pgp to science", "21 March, 2017", "test"],
-                    ["Walk from pgp to biz", "20 March, 2017", "test2"],
-                    ["Walk from biz to computing", "20 March, 2017", "test3"],
-                    ["Walk from computing to clb", "19 March 2017", "test4"],
-                    ["Walk from computing to engining", "18 March 2017", "test2"]]
+    // Just for testing perpose. You can modify this as you wish
+    let routeData = [["Walk from pgp to science", "21 March, 2017", "test.png"],
+                    ["Walk from pgp to biz", "20 March, 2017", "test2.png"],
+                    ["Walk from biz to computing", "20 March, 2017", "test3.png"],
+                    ["Walk from computing to clb", "19 March 2017", "test4.png"],
+                    ["Walk from computing to engining", "18 March 2017", "test2.png"]]
+    let userData = ["Yang Zhuohan", "Singapore, Singapore", ""]
+    
+    // Connects the route list view
     @IBOutlet weak var routeList: UITableView!
+    
+    // Connects the avatar
     @IBOutlet weak var avatar: UIImageView!
+    
+    // Connects user name and his/her location
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var location: UILabel!
+    
+    /// Defines the vibrancy effect view
+    var vibrancyEffectView: UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setCameraView()
         setBlur()
-        routeList.delegate = self
-        routeList.dataSource = self
-        routeList.tableFooterView = UIView(frame: .zero)
-        view.addSubview(routeList)
-        view.addSubview(userName)
-        vibrancyEffectView.addSubview(location)
-        avatar.image = UIImage(named: "gakki.png")
-        avatar.layer.cornerRadius = avatar.bounds.height / 2
-        avatar.layer.masksToBounds = true
-        view.addSubview(avatar)
+        setUserInfo()
+        setRouteList()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
+    /// Sets the camera view as backgound image
     private func setCameraView() {
         let cameraViewController = CameraViewController()
         cameraViewController.view.frame = view.bounds
         view.addSubview(cameraViewController.view)
     }
     
+    /// Sets the blur effect over the background
     private func setBlur() {
         let blurEffect = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
         blurEffect.frame = view.bounds
@@ -56,29 +69,68 @@ class UserProfileViewController: UIViewController {
         blurEffect.contentView.addSubview(vibrancyEffect)
         view.addSubview(blurEffect)
     }
+    
+    /// Sets up the user infomation at the top
+    private func setUserInfo() {
+        setUserAvata()
+        setUserText()
+    }
+    
+    /// Sets the user avata to be the correct image and place it at the
+    /// correct place
+    private func setUserAvata() {
+        
+        // TODO: magic string and magic number
+        var avatarName = "profilePlaceholder.png"
+        if userData[2] != "" {
+            avatarName = userData[2]
+        }
+        avatar.image = UIImage(named: avatarName)
+        avatar.layer.cornerRadius = avatar.bounds.height / 2
+        avatar.layer.masksToBounds = true
+        view.addSubview(avatar)
+    }
+    
+    /// Sets user related texts including user name and location info
+    private func setUserText() {
+        userName.text = userData[0]
+        location.text = userData[1]
+        view.addSubview(userName)
+        vibrancyEffectView.addSubview(location)
+    }
+    
+    /// Sets up the route list table
+    private func setRouteList() {
+        routeList.delegate = self
+        routeList.dataSource = self
+        routeList.tableFooterView = UIView(frame: .zero)
+        view.addSubview(routeList)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
 
 /**
+ An extension that is used to define table view delegate and data source.
  */
 extension UserProfileViewController: UITableViewDelegate, UITableViewDataSource {
     
+    /// Returns the total number of cells in the data table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testData.count
+        return routeData.count
     }
     
+    /// Creates cells for the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "routeListCell", for: indexPath) as! RouteListCell
         
-        cell.routeName.text = testData[indexPath.item][0]
-        cell.routeDescription.text = testData[indexPath.item][1]
-        cell.backgroundImage.image = UIImage(named: testData[indexPath.item][2] + ".png")
-       
+        // TODO: Magic strings and numbers
+        let cell = tableView.dequeueReusableCell(withIdentifier: "routeListCell", for: indexPath) as! RouteListCell
+        cell.routeName.text = routeData[indexPath.item][0]
+        cell.routeDescription.text = routeData[indexPath.item][1]
+        cell.backgroundImage.image = UIImage(named: routeData[indexPath.item][2])
         return cell
     }
     
--- lAyeR/lAyeR/ViewComponents/BasicComponents/Misc/RouteListCell.swift
+++ lAyeR/lAyeR/ViewComponents/BasicComponents/Misc/RouteListCell.swift
@@ -8,20 +8,47 @@
 
 import UIKit
 
+/**
+ This class is used to define a table view that will contain
+ route infomation created by users.
+ Basically, it contains:
+ - route screen shot
+ - route name
+ - route description
+ */
 class RouteListCell: UITableViewCell {
     
-    
+    // Connects the name of the route
     @IBOutlet weak var routeName: UILabel!
+    
+    // Connects the description of the route
     @IBOutlet weak var routeDescription: UILabel!
+    
+    // Connects the background image
     @IBOutlet weak var backgroundImage: UIImageView!
     
+    /// This will be called when the table cell is loaded.
     override func awakeFromNib() {
         super.awakeFromNib()
+        setImageOverlay()
+        disableSelectionStyling()
+    }
+    
+    /// Sets an overlay above the route screen shot so that the texts
+    /// in the front can be seen
+    private func setImageOverlay() {
         let overlay = UIView(frame: backgroundImage.bounds)
-        overlay.backgroundColor = UIColor(red: CGFloat(48/255), green: CGFloat(52/255), blue: CGFloat(65/255), alpha: 0.5)
+        overlay.backgroundColor = UIColor(red: CGFloat(48 / 255),
+                                          green: CGFloat(52 / 255),
+                                          blue: CGFloat(65 / 255),
+                                          alpha: 0.5)
         backgroundImage.layer.cornerRadius = 5
         backgroundImage.layer.masksToBounds = true
         backgroundImage.addSubview(overlay)
+    }
+    
+    /// Disables the selection styling
+    private func disableSelectionStyling() {
         selectionStyle = .none
     }
 
