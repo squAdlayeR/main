8a0d6acc9e6bec1b9a734a8cdd5671964e0cd934
Duan Yichen <victoriaduanyc@gmail.com>
2017-04-01 11:28:41 +0900

    initialization

---
+++ lAyeR/lAyeR/UserProfile.swift
@@ -0,0 +1,38 @@
+//
+//  UserProfile.swift
+//  lAyeR
+//
+//  Created by Victoria Duan on 2017/4/1.
+//  Copyright © 2017年 nus.cs3217.layer. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class UserProfile: Mappable {
+    
+    private(set) var username: String = "lAyeRuser"
+    private(set) var email: String
+    private(set) var avatarRef: String = "profilePlaceholder.png"
+    
+    init(user: User) {
+        self.email = user.email
+    }
+    
+    required init?(map: Map) {
+        guard let username = map.JSON["username"] as? String,
+            let email = map.JSON["email"] as? String,
+            let avatarRef = map.JSON["avatarRef"] as? String else {
+                return nil
+        }
+        self.username = username
+        self.email = email
+        self.avatarRef = avatarRef
+    }
+    
+    func mapping(map: Map) {
+        username <- map["username"]
+        email <- map["email"]
+        avatarRef <- map["avatarRef"]
+    }
+}
