1765ced645177d61b6e5369dd2abd4aab6e0b500
Duan Yichen <victoriaduanyc@gmail.com>
2017-04-08 02:37:14 +0900

    remove unused methods

--- lAyeR/lAyeR/GPSTracker.swift
+++ lAyeR/lAyeR/GPSTracker.swift
@@ -14,17 +14,12 @@ class GPSTracker {
     static let instance = GPSTracker()
     
     private var timer: Timer?
-    private(set) var route: Route?
     private var prevLocation: GeoPoint?
     private var geoManager = GeoManager.getInstance()
     private let defaultLocation = GeoPoint(0, 0)
-    private(set) var distance: Double = 0
-    private(set) var isStarted: Bool = false
     
     func start() {
-        route = Route("New Route")
-        isStarted = true
-        resume()
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(track), userInfo: nil, repeats: true)
     }
     
     @objc func track() {
@@ -32,54 +27,17 @@ class GPSTracker {
         guard currentLocation != defaultLocation else { return }
         guard let prevLocation = prevLocation else {
             self.prevLocation = currentLocation
-            route?.append(CheckPoint(currentLocation, "Source", "", true))
             return
         }
         let deltaDistance = GeoUtil.getCoordinateDistance(prevLocation, currentLocation)
-        guard deltaDistance > 5 else { return }
-        self.distance += deltaDistance
+        guard deltaDistance > 10 else { return }
         self.prevLocation = currentLocation
-        self.route?.append(CheckPoint(currentLocation, "Way Point"))
-    }
-    
-    func insert(name: String, description: String) {
-        guard route != nil else { return }
-        let currentLocation = geoManager.getLastUpdatedUserPoint()
-        let deltaDistance = GeoUtil.getCoordinateDistance(self.prevLocation!, currentLocation)
-        self.distance += deltaDistance
-        self.prevLocation = currentLocation
-        self.route?.append(CheckPoint(currentLocation, name, description, true))
-    }
-    
-    func pause() {
-        timer?.invalidate()
-        timer = nil
-    }
-    
-    func resume() {
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(track), userInfo: nil, repeats: true)
     }
     
     func reset() {
-        isStarted = false
         pause()
-        route = nil
         prevLocation = nil
-        distance = 0
     }
-    
-    func getExportURL() throws -> URL {
-        guard let route = route else {
-            throw GPXError.noRouteFound
-        }
-        //let route = Route.testRoute
-        try GPXManager.save(route: route)
-        let path = try GPXManager.getPath(with: route.name)
-        return URL(fileURLWithPath: path)
-    }
-    
-    func deleteCache(name: String) {
-        GPXManager.delete(routeName: name)
-    }
+
 }
 
