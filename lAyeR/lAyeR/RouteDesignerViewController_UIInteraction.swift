e8d8c5f1a596d85550ba7b2f0a94d64cb194ff34
Yang Zhuohan <billstark1996@gmail.com>
2017-04-11 16:10:34 +0800

    add icons

---
+++ lAyeR/lAyeR/RouteDesignerViewController_UIInteraction.swift
@@ -0,0 +1,21 @@
+//
+//  RouteDesignerViewController_UIInteraction.swift
+//  lAyeR
+//
+//  Created by BillStark on 4/11/17.
+//  Copyright © 2017 nus.cs3217.layer. All rights reserved.
+//
+
+import UIKit
+
+class RouteDesignerViewController_UIInteraction: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
