ffdc7b52bb19b0c9e35961cbff708712bc8b8e28
Yang Zhuohan <billstark1996@gmail.com>
2017-04-12 01:08:18 +0800

    launch screen

--- lAyeR/lAyeR/AppDelegate.swift
+++ lAyeR/lAyeR/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(2)
         GMSPlacesClient.provideAPIKey(AppConfig.apiKey)
         GMSServices.provideAPIKey(AppConfig.apiKey)
         FIRApp.configure()
