0daa65b92866e7bd03e8550af028f4a3fd3b12e6
Yang Zhuohan <billstark1996@gmail.com>
2017-04-13 22:58:30 +0800

    finish refactor camera view

---
+++ lAyeR/lAyeR/CameraViewConstants.swift
@@ -0,0 +1,16 @@
+//
+//  CameraViewConstants.swift
+//  lAyeR
+//
+//  Created by Yang Zhuohan on 13/4/17.
+//  Copyright © 2017 nus.cs3217.layer. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewConstants {
+
+    static let errorMessagePrefix: String = "error: "
+    static let dispatchQueueLabel: String = "VideoDataOutputQueue"
+
+}
