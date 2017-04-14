26dfc7b18ef15c325d8191557b12a53b96b0c586
Yang Zhuohan <billstark1996@gmail.com>
2017-04-14 16:34:08 +0800

    finish refactoring inner view

--- lAyeR/lAyeR/ViewComponents/BasicComponents/InnerViews/InformativeInnerView.swift
+++ lAyeR/lAyeR/ViewComponents/BasicComponents/InnerViews/InformativeInnerView.swift
@@ -20,12 +20,15 @@ class InformativeInnerView: UIView {
     // Defines the real inner view stack
     private var innerViewStack: UIView!
     
+    // stores the subtitle which will be displayed in as the first
+    // line in the view
     private var subtitle: String!
 
     /// Initialization
     /// - Parameters:
     ///     - width: the bounded width of the inner view
     ///     - height: the bounded height of the inner view
+    ///     - subtitle: the subtitle of the inner view
     init(width: CGFloat, height: CGFloat, subtitle: String) {
         let frame = CGRect(x: 0, y: 0, width: width, height: height)
         super.init(frame: frame)
@@ -46,7 +49,7 @@ class InformativeInnerView: UIView {
     /// - Note: by default, there will be subtitle showing that this
     ///     inner view is used to define detailed infomation
     private func initializeContent() {
-        scrollView.frame = self.bounds.insetBy(dx: innerViewSidePadding, dy: 0)
+        scrollView.frame = self.bounds.insetBy(dx: InnerViewConstants.innerViewSidePadding, dy: 0)
         innerViewStack.frame = CGRect(x: 0, y: 0,
                                       width: scrollView.bounds.width,
                                       height: scrollView.bounds.height)
@@ -58,15 +61,16 @@ class InformativeInnerView: UIView {
     }
     
     /// Creates a subtitle for the inner view.
+    /// - Parameter subtitle: the subtitle of the inner view
     /// - Returns: the designed subtitle
     private func createSubtitle(with subtitle: String) -> UILabel {
         let label = UILabel()
-        let frame = CGRect(x: 0, y: innerViewStackMargin,
-                           width: self.bounds.width - innerViewSidePadding * 2,
-                           height: infoPanelTitleHeight)
+        let frame = CGRect(x: 0, y: InnerViewConstants.innerViewStackMargin,
+                           width: self.bounds.width - InnerViewConstants.infoBlockSidePadding * 2,
+                           height: InnerViewConstants.innerViewTitleHeight)
         label.frame = frame
-        label.font = UIFont(name: alterDefaultFontRegular, size: defaultFontSize)
-        label.textColor = infoPanelTitleFontColor
+        label.font = UIFont(name: alterDefaultFontRegular, size: InnerViewConstants.innerViewTitleFontSize)
+        label.textColor = UIColor.gray
         label.text = subtitle
         label.textAlignment = NSTextAlignment.center
         return label
@@ -75,16 +79,23 @@ class InformativeInnerView: UIView {
     /// Inserts a sub info block into the inner view stack
     /// - Parameter view: the view that will be inserted into the view stack
     func insertSubInfo(_ view: UIView) {
+        insertView(view)
+        updateFrame()
+    }
+    
+    /// Purely inserts a view into the view stack
+    /// - Parameter view: the view that is to be inserted into the inner view
+    private func insertView(_ view: UIView) {
+        print(view.frame)
         if let lastSubview = innerViewStack.subviews.last {
             view.frame.origin = CGPoint(x: 0,
                                         y: lastSubview.frame.origin.y
                                             + lastSubview.frame.height
-                                            + innerViewStackMargin)
+                                            + InnerViewConstants.innerViewStackMargin)
             view.frame.size = CGSize(width: view.bounds.width,
-                                     height: view.bounds.height + innerViewStackMargin)
+                                     height: view.bounds.height + InnerViewConstants.innerViewStackMargin)
         }
         innerViewStack.addSubview(view)
-        updateFrame()
     }
     
     /// Updates the scroll view content size after inserting a new view element
