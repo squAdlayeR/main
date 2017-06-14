8d916a157e466d4b58456c22f63654152779ab6a
yicduan <victoriaduanyc@gmail.com>
2017-06-14 23:08:21 +0900

    remove pods from repo

--- lAyeR/Pods/Target Support Files/Pods-lAyeR-lAyeRTests/Pods-lAyeR-lAyeRTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_lAyeR_lAyeRTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_lAyeR_lAyeRTestsVersionString[];
-
--- lAyeR/Pods/Target Support Files/iOS-KML-Framework/iOS-KML-Framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_iOS_KML_Framework : NSObject
-@end
-@implementation PodsDummy_iOS_KML_Framework
-@end
